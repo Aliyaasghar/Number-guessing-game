e66a67dfe9fa444d4364c61d752a87501a535cae
Aliyaasghar <asgharaliya16@gmail.com>
2024-03-28 02:47:21 +0500

    Adding a number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\nWelcome to codewithAliya - alia33-cli-number-guessing-game\n");
+const randonNum = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "personGuessednumber",
+        type: "number",
+        message: "Enter your guess number 1 to 6",
+    }
+]);
+if (answer.personGuessednumber === randonNum) {
+    console.log("yayyy! you guess a correct number");
+}
+else {
+    console.log("Ohoo! you guess a wrong number. Try again");
+}
